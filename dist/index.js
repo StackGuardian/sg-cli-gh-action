ad18de9ce60442a172373f6f14da6b82add39d6b
naveen8801 <naveensharma10d@gmail.com>
2022-02-21 20:26:37 +0530

    init

---
+++ dist/index.js
@@ -0,0 +1,150 @@
+require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 109:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.run = void 0;
+const service = __importStar(__nccwpck_require__(511));
+let api = 'sgu_Efza8ZO5NLe8kT1PfU7KK';
+let endpoint = 'https://testapi.qa.stackguardian.io/api/v1/orgs/policies/wfgrps/testing/wfs/demo-website-aws-s3/wfruns/';
+const context = 'wfruns/';
+let auth = { api_key: api };
+var sgService = new service.WebService(endpoint, context, auth);
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log('in');
+    });
+}
+exports.run = run;
+run();
+
+
+/***/ }),
+
+/***/ 511:
+/***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.WebService = void 0;
+const http = __nccwpck_require__(605);
+const https = __nccwpck_require__(211);
+const url = __nccwpck_require__(835);
+class WebService {
+    constructor(endpoint, context, authorization) {
+        this.baseURL = url.parse(endpoint);
+        if (this.baseURL.path === '/') {
+            this.baseURL.path += context;
+        }
+        else if (this.baseURL.path === `/${context}/`) {
+            this.baseURL.path = `/${context}`;
+        }
+        this.authorization = authorization;
+        this.protocol = this.baseURL.protocol === 'https:' ? https : http;
+        this.protocolLabel = this.baseURL.protocol || 'http:';
+    }
+}
+exports.WebService = WebService;
+
+
+/***/ }),
+
+/***/ 605:
+/***/ ((module) => {
+
+module.exports = require("http");;
+
+/***/ }),
+
+/***/ 211:
+/***/ ((module) => {
+
+module.exports = require("https");;
+
+/***/ }),
+
+/***/ 835:
+/***/ ((module) => {
+
+module.exports = require("url");;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __nccwpck_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId].call(module.exports, module, module.exports, __nccwpck_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __nccwpck_require__(109);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
-import * as core from '@actions/core';
-import * as service from './service';
+import * as core from '@actions/core'
+import * as service from './service'
 
-var sgService = new service.WebService();
-  
+let api: string = 'sgu_Efza8ZO5NLe8kT1PfU7KK'
+
+let endpoint: string =
+  'https://testapi.qa.stackguardian.io/api/v1/orgs/policies/wfgrps/testing/wfs/demo-website-aws-s3/wfruns/'
+
+const context = 'wfruns/'
+
+let auth: service.Authorization = {api_key: api}
+
+var sgService = new service.WebService(endpoint, context, auth)
+
 export async function run() {
-    // TODO: call workflow run api
+  console.log('in')
 }
 
-run();
+run()
