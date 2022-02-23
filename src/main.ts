963ca0b484c7fbc5b00232ce0bfa0f9e955ce9e8
akshat0694 <tandonakshat7@gmail.com>
2022-02-23 15:37:57 +0100

    refactor

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import * as core from '@actions/core'
 import * as service from './service'
 
 // TODO: Get from action inputs. apiKey will be a secret.
-let apiKey: string = 'sgu_Efza8ZO5NLe8kT1PfU7KK'
-let orgId: string = 'policies'
-let workflowGroupId: string = 'testing'
-let workflowId: string = 's3'
+let apiKey: string = core.getInput('apiKey', { required: true });
+let orgId: string = core.getInput('orgId', { required: true });
+let workflowGroupId: string = core.getInput('workflowGroupId', { required: true });
+let workflowId: string = core.getInput('workflowId', { required: true });
 
 let endpoint: string = `https://testapi.qa.stackguardian.io/api/v1/orgs/${orgId}/wfgrps/${workflowGroupId}/wfs/${workflowId}/wfruns/`
 
@@ -17,6 +17,9 @@ var sgService = new service.WebService(endpoint, context, auth)
 
 export async function run() {
   await sgService.WorkflowRun();
+  core.setOutput('msg', 'Workflow Scheduled');
 }
+
+console.log('Triggering Workflow Run')
 
 run()
