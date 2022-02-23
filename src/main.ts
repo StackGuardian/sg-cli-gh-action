4fae080a54c6fd101449f7e5048c3631239086ec
akshat0694 <tandonakshat7@gmail.com>
2022-02-23 15:30:10 +0100

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import * as core from '@actions/core'
 import * as service from './service'
 
-let api: string = 'sgu_Efza8ZO5NLe8kT1PfU7KK'
+// TODO: Get from action inputs. apiKey will be a secret.
+let apiKey: string = 'sgu_Efza8ZO5NLe8kT1PfU7KK'
+let orgId: string = 'policies'
+let workflowGroupId: string = 'testing'
+let workflowId: string = 's3'
 
-let endpoint: string =
-  'https://testapi.qa.stackguardian.io/api/v1/orgs/policies/wfgrps/testing/wfs/s3/wfruns/'
+let endpoint: string = `https://testapi.qa.stackguardian.io/api/v1/orgs/${orgId}/wfgrps/${workflowGroupId}/wfs/${workflowId}/wfruns/`
 
 const context = ''
 
-let auth: service.Authorization = {api_key: api}
+let auth: service.Authorization = {api_key: apiKey}
 
 var sgService = new service.WebService(endpoint, context, auth)
 
