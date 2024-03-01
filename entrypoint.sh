815120581a2a5e5e0530c664a7b05dee29f60840
rixhieloomis <richard.loomis@stackguardian.io>
2024-03-02 00:23:47 +0530

    Formatting entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh -l
-set +x
+
+# Print a message indicating the execution of sg-cli command
+echo "Executing sg-cli command: $@"
+
+# Execute sg-cli command
 sg-cli $@
+
+# Check the exit status of the previous command
+if [ $? -eq 0 ]; then
+    # If the command was successful, print a success message
+    echo "Command executed successfully!"
+else
+    # If the command failed, print an error message
+    echo "Error: Command execution failed!"
+fi
 
 
