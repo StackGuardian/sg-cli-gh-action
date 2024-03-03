f6df26dfa3cda029106b2a56525546a867e85719
AKSHAT TANDON <akshat.tandon@stackguardian.io>
2024-03-03 16:52:04 +0100

    remove unnecessary code

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,3 @@ echo "Executing sg-cli command: $@"
 
 # Execute sg-cli command
 sg-cli $@
-
-# Check the exit status of the previous command
-if [ $? -eq 0 ]; then
-    # If the command was successful, print a success message
-    echo "Command executed successfully!"
-else
-    # If the command failed, print an error message
-    echo "Error: Command execution failed!"
-    exit 1
-fi
-
-
